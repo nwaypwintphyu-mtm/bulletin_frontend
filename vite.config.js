e5e28729d3474df18223689b22f74d6991841e08
nwaypwintphyu-mtm <mtm.nwaypwintphyu@gmail.com>
2025-02-03 14:52:26 +0630

    Add  vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    transform: {
+      '\\.css$': 'vitest-mock-css', // Mock CSS files
+    },
+  },
+});
